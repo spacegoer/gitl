ec820eb0a4c3a906a1837b678416bb167817ba9b
Kwame Okai Addo <48158461+Kwame-Addo@users.noreply.github.com>
2023-12-13 16:57:13 +0000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,33 @@
+// popup
+
+        jQuery(document).ready(function($){
+  
+  window.onload = function (){
+    $(".bts-popup").delay(1000).addClass('is-visible');
+	}
+  
+	//open popup
+	$('.bts-popup-trigger').on('click', function(event){
+		event.preventDefault();
+		$('.bts-popup').addClass('is-visible');
+	});
+	
+	//close popup
+	$('.bts-popup').on('click', function(event){
+		if( $(event.target).is('.bts-popup-close') || $(event.target).is('.bts-popup') ) {
+			event.preventDefault();
+			$(this).removeClass('is-visible');
+		}
+	});
+	//close popup when clicking the esc keyboard button
+	$(document).keyup(function(event){
+    	if(event.which=='27'){
+    		$('.bts-popup').removeClass('is-visible');
+	    }
+    });
+});
+
+
 (function( $ ) {
     "use strict";
     
@@ -738,34 +768,4 @@
 
 
 })( jQuery );
-
-
-// popup
-
-        jQuery(document).ready(function($){
-  
-  window.onload = function (){
-    $(".bts-popup").delay(1000).addClass('is-visible');
-	}
-  
-	//open popup
-	$('.bts-popup-trigger').on('click', function(event){
-		event.preventDefault();
-		$('.bts-popup').addClass('is-visible');
-	});
-	
-	//close popup
-	$('.bts-popup').on('click', function(event){
-		if( $(event.target).is('.bts-popup-close') || $(event.target).is('.bts-popup') ) {
-			event.preventDefault();
-			$(this).removeClass('is-visible');
-		}
-	});
-	//close popup when clicking the esc keyboard button
-	$(document).keyup(function(event){
-    	if(event.which=='27'){
-    		$('.bts-popup').removeClass('is-visible');
-	    }
-    });
-});
 
